bc2ba2f6bc10d1eee98068432654472a53f55842
djon60537-lgtm <djon60537@gmail.com>
2026-04-27 17:02:46 +0600

    Update my_bot.py

--- my_bot.py
+++ my_bot.py
@@ -4,5 +4,9 @@ bot = telebot.TeleBot('8691470503:AAEwP3VuXU0TgX2tp5CWRFrh85mx6hArhz4')
 def start(message):
     bot.send_message(message.chat.id, 'Hi my dear husband')
 
+@bot.message_handler(commands=['love'])
+def you_message(message):
+    bot.send_message(message.chat, "изменилс")
+
 
 bot.polling(none_stop=True)
